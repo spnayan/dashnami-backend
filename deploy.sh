96895a14c1a36e386d61df64d0160f149d2227cc
spnayan <rajnawa93@gmail.com>
2024-07-27 21:51:52 +0545

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,11 @@
+echo 'building project'
 pip install -r requirements.txt
+echo 'requirement'
 
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
+
+echo 'migration complete'
+
 
