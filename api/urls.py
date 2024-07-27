39f1fe49253ea6b70a0b1199a02201437afdf8ca
spnayan <rajnawa93@gmail.com>
2024-07-27 14:21:59 +0545

    migration manual

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PersonViewSet,show_db_settings
+from .views import PersonViewSet,show_db_settings,run_migrations
 
 router = DefaultRouter()
 router.register(r'persons', PersonViewSet)
@@ -8,6 +8,8 @@ router.register(r'persons', PersonViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-        path('settings/', show_db_settings, name='show_db_settings'),
+    path('settings/', show_db_settings, name='show_db_settings'),
+    path('run-migrations/', run_migrations,name='run migrations'),
+
 
 ]
--- api/views.py
+++ api/views.py
@@ -5,8 +5,9 @@ from .serializers import UserSerializer,PersonSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework import viewsets
 # Add this in your views.py for debugging purposes
-from django.http import JsonResponse
+from django.http import JsonResponse,HttpResponse
 from django.conf import settings
+from django.core.management import call_command
 from .models import Person
 
 # Create your views here.
@@ -24,3 +25,10 @@ class PersonViewSet(viewsets.ModelViewSet):
 # Add this in your views.py for debugging purposes
 def show_db_settings(request):
     return JsonResponse(settings.DATABASES)
+# Add this in your views.py for manual migration
+
+
+def run_migrations(request):
+    call_command('migrate')
+    return HttpResponse('Migrations completed')
+
