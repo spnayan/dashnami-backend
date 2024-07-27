3431895d094199e3666d7262e0ae6d46aec9fd81
spnayan <rajnawa93@gmail.com>
2024-07-27 14:04:15 +0545

    add new test setting view

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PersonViewSet
+from .views import PersonViewSet,show_db_settings
 
 router = DefaultRouter()
 router.register(r'persons', PersonViewSet)
+router.register(r'settings', show_db_settings)
+
 
 urlpatterns = [
     path('', include(router.urls)),
--- api/views.py
+++ api/views.py
@@ -4,6 +4,9 @@ from rest_framework import generics
 from .serializers import UserSerializer,PersonSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework import viewsets
+# Add this in your views.py for debugging purposes
+from django.http import JsonResponse
+from django.conf import settings
 from .models import Person
 
 # Create your views here.
@@ -17,3 +20,10 @@ class PersonViewSet(viewsets.ModelViewSet):
     queryset = Person.objects.all()
     serializer_class = PersonSerializer
     permission_classes = [AllowAny]
+
+# Add this in your views.py for debugging purposes
+from django.http import JsonResponse
+from django.conf import settings
+
+def show_db_settings(request):
+    return JsonResponse(settings.DATABASES)
